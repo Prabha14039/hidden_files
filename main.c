2db122918fe50e6b63e08b356a7bb0a245f45965
Prabha14039 <abhinavg760@gmail.com>
2024-07-03 05:38:11 +0000

    file splitter done need to create dummy files

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 #elid _linux_
 	#include<unistd.h>
 #endif
-
+#define cluster_size 512
 long read_file(char *file_path) 
 {
 	char * buffer = NULL;
@@ -60,17 +60,54 @@ long read_file(char *file_path)
  void file_splitter(char * file_path,long m)
 {
 	FILE *f=fopen(file_path,"rb");
-	int parts = (m + 4)/5;
+	if (f == NULL)
+	{
+		fprintf(stderr,"ERROR: the file %s could not be read: %s\n"
+	  ,file_path,strerror(errno));
+		exit(1);
+	}
+
+	int parts = (m/cluster_size);
+	int rem_size=(m%cluster_size);
+
 	for (int i=0;i<parts;i++)
 	{
 		char output_file[100];
 		snprintf(output_file ,sizeof(output_file), "%s.part%d" , file_path, i+1);
 		FILE * outputfile=fopen(output_file,"wb");
-		char buffer [5];
+		if (outputfile == NULL)
+		{
+			fprintf(stderr,"ERROR: the file %s could not be read: %s\n"
+	   ,output_file,strerror(errno));
+			exit(1);
+
+		}
+		char buffer [cluster_size];
 		size_t read_buffer=fread(buffer,1,sizeof(buffer),f);
 		fwrite(buffer,1 ,read_buffer,outputfile);
 		fclose(outputfile);
 	}
+
+	if (rem_size>0)
+	{	
+		char rem_file[100];
+		snprintf(rem_file ,sizeof(rem_file), "%s.part_remfile" , file_path);
+		FILE * rem=fopen(rem_file,"wb");
+		if (rem == NULL)
+		{
+			fprintf(stderr,"ERROR: the file %s could not be read: %s\n"
+	   ,rem_file,strerror(errno));
+			exit(1);
+		}
+		char buffer [rem_size];
+		size_t read_buffer=fread(buffer,1,sizeof(buffer),f);
+		fwrite(buffer,1 ,read_buffer,rem);
+		fclose(rem);
+	}
+
+	fclose(f);
+	printf("the %s file have sucessfully been split : \n %d :size -- %d bytes \n 1 :size -- %d bytes \n",file_path,parts,cluster_size,rem_size);
+	
 }
 
 int main(int argc,char **argv)
@@ -90,8 +127,6 @@ int main(int argc,char **argv)
 	}
 	file_splitter(input_file_path,content_size);
 
-
-	
 	//printf("the size of %s file is :%ld bytes\n",input_file_path,content);
 	
 	//printf("hello this is the project");

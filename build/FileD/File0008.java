44c670f4266598d450b0c316d4c4e0b19ffd3abd
abhi <138896016+Prabha14039@users.noreply.github.com>
2024-07-30 14:42:45 +0530

    all done

--- build/FileD/File0008.java
+++
@@ -1,8 +0,0 @@
-ring description = fileSystemView.getSystemTypeDescription(root);
-            System.out.println(description);
-            if (description != null && (description.contains("Removable") || description.contains("USB Drive"))) {
-                // Use the path of the removable drive
-                String drivePath = root.getAbsolutePath();
-                //      System.out.println("Path of removable drive: " + drivePath);
-                // Check file system type
-                String fileSystemType 
--- build/FileD/File0012.java
+++
@@ -1,9 +0,0 @@
-  drivePath = drivePath.endsWith("\\") ? drivePath.substring(0, drivePath.length() - 1) : drivePath;
-
-            // Run the 'fsutil fsinfo volumeinfo' command to get file system details
-            Process process = Runtime.getRuntime().exec("fsutil fsinfo volumeinfo " + drivePath);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line;
-
-            // Print the output for debugging
-            //          System.out.println
